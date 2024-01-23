5ebc9e026e4d9a891e34a0b43d2a8a7487763794
Guilherme Solino <guilherme.solino@gmail.com>
2024-01-23 07:54:27 -0300

    O Frontend está repaginado e submit picks é atualizado no db

--- run.py
+++ run.py
@@ -19,10 +19,13 @@ if __name__ == "__main__":
 # git commit -m "Descrever o que foi feito"
 # git branch
 # git checkout -b feature/html
-# git push
+# git push --set-upstream origin novo_frontend
 
 ## comando para definir a variável de ambiente FLASK_APP para indicar ao Flask qual é o arquivo de entrada da sua aplicação
 # $env:FLASK_APP = "D:\TENIS\SF_app\tennis_app"
 # flask db migrate -m "Add qf_number to Player"
 # flask db upgrade
+
+# atualizar o arquivo requirements.txt    
+# pip freeze > requirements.txt
     
